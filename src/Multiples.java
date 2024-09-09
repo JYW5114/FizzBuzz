90de41e60c9122165e6d8f577d87b92c76c5194e
JYW5114 <elowenyiwei.jiang@mail.utoronto.ca>
2024-09-09 18:42:23 -0400

    task4&5

---
+++ src/Multiples.java
@@ -0,0 +1,21 @@
+public class Multiples {
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 1; i < 1000; i++) {
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            // Print our appropriate result.
+
+            if (divisibleBy3) {
+
+                count++;
+
+            } else if (divisibleBy5) {
+
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
